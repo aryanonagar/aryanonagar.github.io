79c910a1b2e83ea4eff15276ba4a4e33dd5b7ad4
Aryan Nagar <aryanonagar@gmail.com>
2024-06-16 13:05:59 +0530

    new site
    new site

---
+++ logic.js
@@ -0,0 +1,19 @@
+// scroll indicator
+document.addEventListener('scroll', function() {
+    var scrollOne = document.getElementById('scrollOne');
+    scrollOne.classList.add('hidden');
+});
+
+// button click
+document.getElementById('project-button-one').addEventListener('click', function() {
+    window.location.href = 'https://github.com/aryanonagar';
+});
+document.getElementById('project-button-two').addEventListener('click', function() {
+    window.location.href = 'https://github.com/aryanonagar';
+});
+document.getElementById('project-button-three').addEventListener('click', function() {
+    window.location.href = 'https://github.com/aryanonagar';
+});
+document.getElementById('project-button-four').addEventListener('click', function() {
+    window.location.href = 'https://github.com/aryanonagar';
+});
